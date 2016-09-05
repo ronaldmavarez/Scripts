6b5820362dee5b4ac4a0f477161d81caf243420e
ronaldmavarez <mavarez.ronald@gmail.com>
2016-09-05 16:58:03 +0200

    New update

--- SharePoint/70-489/SPConsole/SPConsole/UpdateUserProfileImageCSOM.js
+++ SharePoint/70-489/SPConsole/SPConsole/UpdateUserProfileImageCSOM.js
@@ -4,14 +4,16 @@ var clientContext = new SP.ClientContext.get_current();
 var peopleManager = new SP.UserProfiles.PeopleManager(clientContext);
 var userProfileProperties = peopleManager.getMyProperties()
 var managerProfileProperties = [];
+var profilePropertyNames = [];
+var targetUser;
 
 clientContext.load(userProfileProperties);
 clientContext.executeQueryAsync(GetManagerObject, failure);
 
 function GetManagerObject() {
     var profilePropertyNames = ["Manager"];
-    //var targetUser = userProfileProperties.get_accountName();
-    var targetUser = "administrator";
+    targetUser = userProfileProperties.get_accountName();
+    //var targetUser = "administrator";
     var userProfilePropertiesForUser = new SP.UserProfiles.UserProfilePropertiesForUser(clientContext, targetUser, profilePropertyNames);
 
     managerProfileProperties = peopleManager.getUserProfilePropertiesFor(userProfilePropertiesForUser);
@@ -22,8 +24,9 @@ function GetManagerObject() {
 
 
 function GetManagerDetails() {
-    var profilePropertyNames = ["PreferredName", "WorkPhone", "Title"];
-    var targetUser = managerProfileProperties[0];
+    profilePropertyNames = ["PreferredName", "WorkPhone", "Title"];
+    //var targetUser = managerProfileProperties[0];
+
     var userProfilePropertiesForUser = new SP.UserProfiles.UserProfilePropertiesForUser(clientContext, targetUser, profilePropertyNames);
 
     managerProfileProperties = peopleManager.getUserProfilePropertiesFor(userProfilePropertiesForUser);
@@ -33,26 +36,16 @@ function GetManagerDetails() {
 }
 
 function showDetails() {
-    console.log(managerProfileProperties[0] + " - " + managerProfileProperties[1] + " " + managerProfileProperties[2]);
-}
 
+    for(var i=0; i < managerProfileProperties.length; i++){
+        console.log("The value is: " + managerProfileProperties[i]);
+    }
+    
+    //console.log(managerProfileProperties[0] + " - " + managerProfileProperties[1] + " - " + managerProfileProperties[2]);
+    //console.log("The user is: " + );
+}
 
 function failure() {
     console.log("Failed");
 };
 
-
-
-function GetManagerDetails() {
-    var clientContext = new SP.ClientContext.get_current();
-    var peopleManager = new SP.UserProfiles.PeopleManager(clientContext);
-
-    var profilePropertyNames = ["PreferredName", "WorkPhone", "Title"];
-    var targetUser = "administrator";
-    var userProfilePropertiesForUser = new SP.UserProfiles.UserProfilePropertiesForUser(clientContext, targetUser, profilePropertyNames);
-
-    managerProfileProperties = peopleManager.getUserProfilePropertiesFor(userProfilePropertiesForUser);
-
-    clientContext.load(userProfilePropertiesForUser);
-    clientContext.executeQueryAsync(userProfilePropertiesForUser);
-}
---
+++ SharePoint/RegionalSettingsInAllSites.ps1
@@ -0,0 +1,85 @@
+<#
+THIS SCRIPT IS TO UPDATE ALL THE WEB SITES 
+AND SITE COLLECTION ON A SHAREPOINT APP.
+
+THIS HAS BEEN TESTED ON SP2010 WITH POWERSHELL 2.0
+THIS MAY WORK ON BEFORE SP VERSIONS, TO CHECK THE POWERSHELL
+VERSION IN YOUR SERVER USE $host.version
+
+Ronald Mavarez
+Peanuts AS
+06/13
+#>
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+[Reflection.Assembly]::LoadWithPartialName("System.Globalization") | out-null
+clear-host
+
+#THIS IS THE VARIABLE WHERE YOU SET YOUR SITE
+$mysiteUrl = ""
+
+#reset the arrays
+$goodSiteURLs = @() 
+$errorSiteURLs = @()
+$siteURLs = @()
+
+<#
+GETTING ALL THE URLs, THIS INCLUDE SITE COLLECTION AND WEBs
+#>
+#The code below is the one that takes the URL of the site collections
+$SiteCols = Get-SPWebApplication $mysiteURL | Get-SPSiteAdministration -Limit ALL | Select URL
+foreach ($siteCol in $SiteCols){
+    $currentSiteURL = $siteCol.url
+    
+    #just add the item if you have a URL
+    if($currentSiteURL){
+        $siteURLs += @($currentSiteURL)
+    }#end if
+}#end foreach
+
+#The code below is the one that takes the URL of the sites
+$Sites = Get-SPSite $mysiteUrl | Get-SPWeb -Limit All | Select URL
+foreach ($siteCol in $Sites){
+    $currentSiteURL = $siteCol.url
+    
+    #just add the item if you have a URL
+    if($currentSiteURL){
+        $siteURLs += @($currentSiteURL)
+    }#end if
+}#end foreach
+
+#HERE IS WHERE THE ACTION BEGINS!
+foreach ($currentSiteURL in $siteURLs){
+    
+    #just works if you have a URL
+    if($currentSiteURL){
+        #try catch inside of a foreach???
+        try {
+            Get-SPWeb $currentSiteURL -limit all |%{
+                #Setting the values to the properties
+                $_.Locale = [System.Globalization.CultureInfo]::GetCultureInfo("nb-NO") #Most used nb-NO / en-GB
+                $_.RegionalSettings.WorkDayStartHour = 480 #Set hours with minutes, ex: 60 * 8 => 480
+                $_.RegionalSettings.WorkDayEndHour = 960 #same as before
+                $_.RegionalSettings.TimeZone.ID = 4	#4 = (UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna
+                $_.RegionalSettings.CalendarType = 1 #Gregorian
+                $_.RegionalSettings.WorkDays = 62 #0111110 Mon-Fri, this is a binary sum of 7 digits
+
+                $_.Update()
+                $goodSiteURLs += @($currentSiteURL) 
+            }#end GET-SPWeb
+        }catch{
+          $errorSiteURLs += @($currentSiteURL)
+        }#end try
+    }#end if when URL
+}#foreach
+
+if ($errorSiteURLs.length -gt 0 ){
+    write-host "There was a problem with the site (s) listed below, you have to fix it manually at _layouts/regionalsetng.aspx"
+    write-host ""
+    foreach ($site in $errorSiteURLs){
+        write-host $site
+    }#foreach
+}#end if
+
+write-host "The script has sucesfully update" $goodSiteURLs.length"/"$siteURLs.length "site(s) with" $errorSiteURLs.length "error (s)"
