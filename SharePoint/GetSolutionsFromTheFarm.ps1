583ac55333b58a1d04fd3fe4680e01bcd2bcde9b
ronaldmavarez <mavarez.ronald@gmail.com>
2016-10-20 10:11:50 +0200

    Updates in the code to download all or specified WSPs

--- SharePoint/GetSolutionsFromTheFarm.ps1
+++ SharePoint/GetSolutionsFromTheFarm.ps1
@@ -1,11 +1,47 @@
-#SharePoint script for get the solutions from the farm or Central Administration, you name it.
-
-[void][reflection.assembly]::Loadwithpartialname("Microsoft.SharePoint") | out-null
 $directory = "c:\temp\" #make sure the path exist or you have access
-$solutionNames = @("Solution1.wsp", "Solution2.wsp", "Solution3.wsp") #write the name of the solutions you need
+#$solutionNames = @("Solution1.wsp", "Solution2.wsp", "Solution3.wsp") #write the name of the solutions you need
+$solutionNames = ""
 
-$farm = Get-SPFarm
-foreach ($solutionName in $solutionNames) {
-	$file = $farm.Solutions.Item($solutionName).SolutionFile
-	$file.SaveAs($directory + $solutionName)
+Write-Host Exporting solutions to $directory  
+
+#When looking for specifics solutions
+if ($solutionNames.length -gt 0){ 
+	$farm = Get-SPFarm
+	foreach ($solutionName in $solutionNames) {
+		if($farm.Solutions.Item($solutionName)){
+			$solution = $farm.Solutions.Item($solutionName)
+			$title = $solution.Name  
+			$filename = $solution.SolutionFile.Name 
+			
+			Write-Host "Exporting ‘$title’ as …\$filename"
+			
+			try {  
+				$solution.SolutionFile.SaveAs("$directory\$filename")  
+			}  
+			catch  
+			{  
+				Write-Host " – error : $_" -foreground red  
+			}  
+		}
+	}
+	
+	Write-Host "Exported all the WSP specified and found in server" -foreground green
+}else { #When looking for all he solutions in the server
+	
+	foreach ($solution in Get-SPSolution)  {   
+		$title = $solution.Name  
+		$filename = $solution.SolutionFile.Name 
+		
+		Write-Host "Exporting ‘$title’ as …\$filename"
+
+		try {  
+			$solution.SolutionFile.SaveAs("$directory\$filename")  
+		}  
+		catch  
+		{  
+			Write-Host " – error : $_" -foreground red  
+		}  
+	}
+	
+	Write-Host "Exported all the WSP found in the server" -foreground green
 }
