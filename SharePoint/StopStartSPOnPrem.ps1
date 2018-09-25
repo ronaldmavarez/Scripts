b7d204243fc7f6c528c30add7e3728def2d14568
Ronald Mavarez <mavarez.ronald@gmail.com>
2018-09-25 10:33:18 +0200

    Stop Start SP OnPremises services

---
+++ SharePoint/StopStartSPOnPrem.ps1
@@ -0,0 +1,13 @@
+net stop SPAdminV4
+net stop SPTimerV4
+net stop SPSearchHostController
+net stop OSearch15
+net stop SPTraceV4
+net stop MSSQLSERVER
+
+net start MSSQLSERVER
+net start SPAdminV4
+net start SPSearchHostController
+net start OSearch15
+net start SPTraceV4
+net start SPTimerV4
